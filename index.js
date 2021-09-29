cf6cb09e4991b06a843ba6cf6cc3cd43cf33294e
Emmanuel Izilov <em5750@gmail.com>
2021-09-29 17:35:25 -0400

    Core lab solutions added

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("Hello world")
+
+const message = document.querySelector('#message')
+
+function addMovie(event){
+    event.preventDefault()
+    let inputField = document.querySelector('input')
+    console.log(inputField)
+
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    
+    movieTitle.textContent = inputField.value
+    // console.log(movieTitle)
+    movieTitle.addEventListener('click', crossOffMovie)
+
+    movie.appendChild(movieTitle)
+    // console.log(movie)
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+
+
+    const list = document.querySelector('ul')
+    // movie.appendChild(ul) //Wrong way
+    list.appendChild(movie)
+
+
+    inputField.value = '';
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted!'
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', addMovie)
+
+function crossOffMovie(event){
+    event.target.classList.toggle("checked")
+
+    if (event.target.classList.contains('checked')){
+        message.textContent = 'Movie watched!'
+    } else {
+        message.textContent = 'Movie added back!'
+
+    }
+}
+
